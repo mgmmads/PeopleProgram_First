ac0ab384bc9584a7ad9bfc5662768665e7fbb062
mgmmads <andersenmads150693@hotmail.com>
2016-01-22 20:35:49 +0100

    created UI and Database + Table
    Created a simple UI that Uses most of what we have learned in class. +
    Experimenting with creating local database in Visual Studio.

---
+++ People Program/dbo.Table.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[People]
+(
+	[Id] INT NOT NULL PRIMARY KEY,
+	[FirstName] nvarchar NOT NULL,
+	[SecondName] nvarchar NULL,
+	[LastName] nvarchar NOT NULL,
+	[Gender] int NOT NULL,
+	[DateOfBirth] Date NULL,
+	[Address] nvarchar NOT NULL,
+	[CurrentCity] nvarchar NOT NULL,
+	[BirthCountry] nvarchar NULL,
+	[CurrentCountry] nvarchar NULL
+)
